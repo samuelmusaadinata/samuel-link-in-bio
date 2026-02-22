f4f58dd7cbf1a6754c62b2e6597233e0f7a506ec
Samuel Musa Adinata <musaadinata5@gmail.com>
2026-02-22 07:29:16 +0700

    Update vite.config.js with new Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: '/samuel-link-in-bio/',
-  plugins: [vue()],
-});
+})
